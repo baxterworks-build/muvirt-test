150366bc807554d2f8d7228802e764f0c871035d
Adam Baxter <voltagex@voltagex.org>
2021-01-25 16:11:34 +1100

    Add awk

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo this line is just for drone.io
-apt update; apt -y --no-install-recommends install curl ca-certificates xz-utils make build-essential libncurses-dev git unzip bzip2 wget python3 file
+apt update; apt -y --no-install-recommends install curl ca-certificates xz-utils make build-essential libncurses-dev git unzip bzip2 wget python3 file gawk
 
 curl -L https://archive.traverse.com.au/pub/traverse/software/muvirt/branches/master/latest/image/muvirt-sdk-SNAPSHOT-layerscape-armv8_64b_gcc-8.4.0_musl.Linux-x86_64.tar.xz | tar -Jxf -
 
