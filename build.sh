833d9a832fe29939ddfdd742ea7649b5db21de4d
Adam Baxter <voltagex@voltagex.org>
2021-01-25 16:31:08 +1100

    *shakes fist at Bash*

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+set -eou pipefail
 echo this line is just for drone.io
 apt update; apt -y --no-install-recommends install curl ca-certificates xz-utils make build-essential libncurses-dev git unzip bzip2 wget python3 file gawk
 
 curl -L https://archive.traverse.com.au/pub/traverse/software/muvirt/branches/master/latest/image/muvirt-sdk-SNAPSHOT-layerscape-armv8_64b_gcc-8.4.0_musl.Linux-x86_64.tar.xz | tar -Jxf -
-cp -v ten64.config muvirt*/.config
+
+SOURCE=$(readlink -f muvirt*)
+cp -v ten64.config $SOURCE/.config
 
 pushd muvirt*
 
