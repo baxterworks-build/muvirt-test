d656ac3c3c78ef15173f9167f99b369c810dab0f
Adam Baxter <voltagex@voltagex.org>
2021-01-25 16:01:34 +1100

    xz-utils not xz

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo this line is just for drone.io
-apt update; apt -y install curl xz
+apt update; apt -y install curl xz-utils
 
 curl -L https://archive.traverse.com.au/pub/traverse/software/muvirt/branches/master/latest/image/muvirt-sdk-SNAPSHOT-layerscape-armv8_64b_gcc-8.4.0_musl.Linux-x86_64.tar.xz | tar -jvxf -
 
