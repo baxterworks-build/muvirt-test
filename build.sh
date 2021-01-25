0d6081ccae8cdac8a7f879dc11d6016c0ee886e0
Adam Baxter <voltagex@voltagex.org>
2021-01-25 15:56:53 +1100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+echo this line is just for drone.io
 apt update; apt -y install curl xz
 
 curl -L https://archive.traverse.com.au/pub/traverse/software/muvirt/branches/master/latest/image/muvirt-sdk-SNAPSHOT-layerscape-armv8_64b_gcc-8.4.0_musl.Linux-x86_64.tar.xz | tar -jvxf -
