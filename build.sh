8a1d128f373e00463c10a37003e8a4252ff9d8e7
Adam Baxter <voltagex@voltagex.org>
2021-01-25 16:05:22 +1100

    j or J?

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 echo this line is just for drone.io
 apt update; apt -y --no-install-recommends install curl ca-certificates xz-utils make
 
-curl -L https://archive.traverse.com.au/pub/traverse/software/muvirt/branches/master/latest/image/muvirt-sdk-SNAPSHOT-layerscape-armv8_64b_gcc-8.4.0_musl.Linux-x86_64.tar.xz | tar -jvxf -
+curl -L https://archive.traverse.com.au/pub/traverse/software/muvirt/branches/master/latest/image/muvirt-sdk-SNAPSHOT-layerscape-armv8_64b_gcc-8.4.0_musl.Linux-x86_64.tar.xz | tar -Jvxf -
 
 pushd muvirt*
 
