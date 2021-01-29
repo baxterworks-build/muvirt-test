8389a4d32f2e33e7fd2b2f391d2f9610c3c06080
Adam Baxter <voltagex@voltagex.org>
2021-01-29 22:52:57 +1100

    Build specific packages

--- build.sh
+++ build.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
-echo this line is just for drone.io
-apt update; apt -y --no-install-recommends install curl ca-certificates xz-utils make build-essential libncurses-dev git unzip bzip2 wget python3 file gawk
+set -eou pipefail
+if ! [ -z ${DRONE+x} ]; then
+	apt update; apt -y --no-install-recommends install curl ca-certificates xz-utils make build-essential libncurses-dev git unzip bzip2 wget python3 file gawk
+fi
 
-curl -L https://archive.traverse.com.au/pub/traverse/software/muvirt/branches/master/latest/image/muvirt-sdk-SNAPSHOT-layerscape-armv8_64b_gcc-8.4.0_musl.Linux-x86_64.tar.xz | tar -Jxf -
-cp -v ten64.config muvirt*/.config
+if ! [ -d muvirt* ]; then #no SDK extracted yet
+	curl -L https://archive.traverse.com.au/pub/traverse/software/muvirt/branches/master/latest/image/muvirt-sdk-SNAPSHOT-layerscape-armv8_64b_gcc-8.4.0_musl.Linux-x86_64.tar.xz | tar -Jxf -
+fi
 
 pushd muvirt*
+make defconfig
 
 ./scripts/feeds update -a
-./scripts/feeds install -a
-
-make -j16
+for p in i2c-tools luci-app-minidlna luci-app-samba4 luci-app-vnstat; do
+	./scripts/feeds install $p
+	make -j `nproc` package/$p/compile
+done
